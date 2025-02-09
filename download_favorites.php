23589c9ef880a7bc4e5a2797a74aefba6ba77f61
ALEKSANDRO ALVES DA ROCHA JUNIOR <97574073+aajunior43@users.noreply.github.com>
2025-02-09 19:19:09 -0300

    Delete download_favorites.php

--- download_favorites.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="favorites.txt"');
-header('Content-Transfer-Encoding: binary');
-
-// Lê e envia o arquivo
-readfile('favorites.txt');
-?>
